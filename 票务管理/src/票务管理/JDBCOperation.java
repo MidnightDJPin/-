03cb93cdc741d25f6c48c5fad82c8ef7b0931921
MidnightDJPin <abc19990101@126.com>
2018-07-06 11:37:05 +0800

    add connector

--- 票务管理/src/票务管理/JDBCOperation.java
+++ 票务管理/src/票务管理/JDBCOperation.java
@@ -8,5 +8,21 @@ import java.sql.SQLException;
 import com.mysql.jdbc.*;
 
 public class JDBCOperation {
-	
+	private static Connection getConnection() {
+		String driver = "com.mysql.jdbc.Driver";
+		String url = "jdbc:mysql://localhost:3306/ticket_system";
+		String username = "root";
+		String password = "";
+		Connection connection = null;
+		try {
+			Class.forName(driver);
+			connection = (Connection) DriverManager.getConnection(url, username,
+					password);
+		} catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return connection;
+	}
 }
